ed4d0b19e6ab2e43f75aee85c95926992103b0de
Şeymanur Diktaş <seymanurdiktas@gmail.com>
2021-02-22 20:47:54 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.DTOs;
+using Core.Utilities.Results;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+
+        IDataResult<List<Product>> GetAll();
+        IDataResult<List<Product>> GetAllByCategory(int Id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IDataResult<Product> GetById(int productId);
+        IResult Add(Product product);
+
+    }
+}
